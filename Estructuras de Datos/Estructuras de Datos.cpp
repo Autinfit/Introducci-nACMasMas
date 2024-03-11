ec7681684ae338209c09cdbe6ed4cdb28ec929bd
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-03-11 14:09:09 -0300

    Comienzo del proyecto.

---
+++ Estructuras de Datos/Estructuras de Datos.cpp
@@ -0,0 +1,27 @@
+// Estructuras de Datos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+// #include <iostream>
+
+// EN ESTA CLASE DEL MAIN UTILIZAREMOS ESPACIO DE NOMBRES.
+
+// using namespace std;
+
+int main()
+{
+    // ESTA ES LA CLASE DEL MAIN POR DEFECTO.
+
+    // REALIZAREMOS OPERACIONES ALGEBRAICAS EN ESTE MÉTODO.
+
+    int x = 4; // "x" ES IGUAL A 4.
+    x++; // INCREMENTA SU VALOR AL PRINCIPIO DE ESTA VARIABLE YA DECLARADA.
+    int y = 8; // "y" ES IGUAL A 8.
+
+    // PARA PRACTICAR UN POCO CON ESTE LENGUAJE DE PROGRAMACIÓN, VAMOS A CREAR UN CICLO "for".
+
+    for (int i = 0; i < 10; i++) // LOS NÚMEROS YA DECLARADOS EN ESTA ITERACIÓN ESTARÁN ENTRE EL 0 Y EL 9.
+    {
+        y++; // INCREMENTA SU VAOR EN "y".
+    }
+};
+
